433d05b61898e8dcaf50453b647d65655bb03d75
DayKui <15658052167@163.com>
2019-07-30 21:21:27 +0800

    push current files

---
+++ Assets/Scripts/ECS/Settings/SpawnOneBlock.cs
@@ -0,0 +1,67 @@
+﻿using Unity.Entities;
+using Unity.Rendering;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using Unity.Collections;
+
+namespace Minecraft
+{
+    public class SpawnOneBlock : MonoBehaviour
+    {
+
+        public static EntityArchetype BlockArchetype;
+
+        [Header("Mesh Info")]
+        public Mesh blockMesh;
+
+        [Header("Nature Block Type")]
+        public Material blockMaterial;
+
+        public EntityManager manager;
+        public Entity entities;
+        public GameObject Prefab_ref;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Initialize()
+        {
+
+            EntityManager manager = World.Active.GetOrCreateManager<EntityManager>();
+
+            // Create an archetype for basic blocks.
+            BlockArchetype = manager.CreateArchetype(
+                typeof(Position)
+            );
+        }
+
+        //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        void Start()
+        {
+            manager = World.Active.GetOrCreateManager<EntityManager>();
+
+            Entity entities = manager.CreateEntity(BlockArchetype);
+            manager.SetComponentData(entities, new Position { Value = new int3(2, 0, 0) });
+            manager.AddComponentData(entities, new BlockTag { });
+
+            manager.AddSharedComponentData(entities, new MeshInstanceRenderer
+            {
+                mesh = blockMesh,
+                material = blockMaterial
+            });
+
+
+            //use prefab to create a entity
+            if (Prefab_ref)
+            {
+                NativeArray<Entity> entityArray = new NativeArray<Entity>(3, Allocator.Temp);
+                manager.Instantiate(Prefab_ref, entityArray);//把预制体转为Entity
+
+                manager.SetComponentData(entityArray[0], new Position { Value = new float3(4, 0f, 0f) });
+                manager.SetComponentData(entityArray[1], new Position { Value = new float3(6, 0f, 0f) });
+                manager.SetComponentData(entityArray[2], new Position { Value = new float3(-2, 0f, 0f) });
+
+                entityArray.Dispose();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MyScripts/RockMaterialMonitor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RockMaterialMonitor : MonoBehaviour {
+
+    [SerializeField] private int pictureWidth = 200;
+    [SerializeField] private int pictureHeight = 200;
+    [SerializeField] private float xOrg = 0f;
+    [SerializeField] private float yOrg = 0f;
+    //柏林噪声缩放值越大，柏林噪声计算越密集
+    [SerializeField] private float scale = 20;
+
+    private Texture2D noiseTex;
+
+    private Color[] pix;
+    private MeshRenderer meshRender;
+
+    private void Start()
+    {
+        meshRender = GetComponent<MeshRenderer>();
+        noiseTex = new Texture2D(pictureWidth,pictureHeight);
+        pix = new Color[noiseTex.width*noiseTex.height];
+        meshRender.material.mainTexture = noiseTex;
+    }
+
+    private void Update()
+    {
+        calcNoise();
+    }
+
+    //计算噪声
+    private void calcNoise()
+    {
+        float y = 0f;
+        while (y<noiseTex.height)
+        {
+            float x = 0;
+            while (x<noiseTex.width)
+            {
+                //计算出x的采样值
+                float xCoord = xOrg + x / noiseTex.width * scale;
+                //y的采样值
+                float yCoord = yOrg + y / noiseTex.height * scale;
+                //用计算出的采样值计算柏林噪声
+                float sample = Mathf.PerlinNoise(xCoord,yCoord);
+                pix[Convert.ToInt32(y * noiseTex.width + x)] = new Color(sample,sample,sample);
+                x++;
+            }
+            y++;
+        }
+        noiseTex.SetPixels(pix);
+        noiseTex.Apply();
+    }
+}
